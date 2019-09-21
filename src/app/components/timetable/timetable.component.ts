2780a58775e91a05e043308431f9e684ab1c7396
Adur Marques <adur.marques@gmail.com>
2019-09-21 16:23:06 +0200

    Timetable functionality x

--- src/app/components/timetable/timetable.component.ts
+++ src/app/components/timetable/timetable.component.ts
@@ -8,11 +8,20 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./timetable.component.scss']
 })
 export class TimetableComponent implements OnInit {
-    rows;
+    rows: number;
+    rowIndex: number;
     modal;
+    selectedSubject;
+    editionActivatedInRow;
+    editionActivatedInSubject
 
     subjects: Subject[] =
         [
+            {
+                id: 0,
+                name: 'Libre',
+
+            },
             {
                 id: 1,
                 name: 'MME',
@@ -44,76 +53,78 @@ export class TimetableComponent implements OnInit {
             }
         ];
 
-    timetable: Timetable = {
-        id: 1,
-        user_id: 1,
-        rows: 7,
-        hours: ['8:30-9:25', '9:25-10:30', '10:20 - 11:15', '11:45 - 12:40', '12:40 - 13:35', '13:35 - 14:30'],
-        subjects: [
-            [
-                {
-                    id: 1,
-                    subject_id: 1,
-                },
-                {
-                    id: 2,
-                    subject_id: 2,
-                },
-                {
-                    id: 3,
-                    subject_id: 3,
-                },
-                {
-                    id: 4,
-                    subject_id: 6,
-                },
-                {
-                    id: 5,
-                    subject_id: 5,
-                }
-            ],
-            [
-                {
-                    id: 6,
-                    subject_id: 1,
-                },
-                {
-                    id: 7,
-                    subject_id: 2,
-                },
-                {
-                    id: 8,
-                    subject_id: 6,
-                },
-                {
-                    id: 9,
-                    subject_id: 4,
-                },
-                {
-                    id: 10,
-                    subject_id: 5,
-                }
-            ]
-        ]
-    };
+    timetable: Timetable;
 
     constructor(
         private modalService: NgbModal
-    ) {
-        this.rows = Array(this.timetable.rows).fill(0).map((x, i) => i);
-    }
+    ) { }
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 
     openModal(content) {
         this.modal = this.modalService.open(content, { size: 'xl' });
     }
 
-    findSubject(subject_id) {
-        if (subject_id !== 0 || subject_id !== null) {
-            return this.subjects.find(subject => subject.id === subject_id).name;
+    createTable() {
+        this.timetable = {
+            id: 1,
+            user_id: 1,
+            rows: this.rows,
+            hours: ['8:30-9:25', '9:25-10:30', '10:20 - 11:15', '11:15 - 11:45', '11:45 - 12:40', '12:40 - 13:35', '13:35 - 14:30'],
+            subjects: []
         }
-        return;
+
+        this.addRows(+this.rows);
     }
+
+    addRows(rows) {
+        let subjects;
+        this.timetable.subjects;
+
+        let number = 1;
+
+        for (let e of Array(rows)) {
+            subjects = [];
+
+            for (let i of Array(5)) {
+                subjects.push(
+                    {
+                        id: null,
+                        cell: number++,
+                        subject_id: 0,
+                    }
+                );
+            }
+
+            this.timetable.subjects.splice(this.rowIndex, 0, subjects);
+        }
+    }
+
+    deleteRow(index) {
+        this.timetable.subjects.splice(index, 1);
+    }
+
+    activateEdition(rowIndex, subjectIndex) {
+        this.editionActivatedInRow = rowIndex;
+        this.editionActivatedInSubject = subjectIndex;
+    }
+
+    editionActivated(rowIndex, subjectIndex) {
+        if (this.editionActivatedInRow == rowIndex && this.editionActivatedInSubject == subjectIndex) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    updateCell(rowIndex, subjectIndex) {
+        this.timetable.subjects[rowIndex][subjectIndex].subject_id = +this.selectedSubject;
+
+        this.selectedSubject = null;
+    }
+
+    findSubject(subject_id) {
+        return this.subjects.find(subject => subject.id === subject_id);
+    }
+
 }
